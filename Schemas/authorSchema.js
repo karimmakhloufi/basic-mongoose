38978af4a866164e4dda988324dd23af8ad89ecf
Karim Makhloufi <karim@kazoo.agency>
2020-11-25 17:38:27 +0100

    populate, ref, save, debug

--- Schemas/authorSchema.js
+++ Schemas/authorSchema.js
@@ -2,6 +2,7 @@ import mongoose from "mongoose";
 
 const authorSchema = new mongoose.Schema({
   name: String,
+  posts: [{ type: mongoose.Schema.Types.ObjectId, ref: "Post" }],
 });
 
 export default authorSchema;
--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import bodyParser from "body-parser";
 
+mongoose.set("debug", true);
 mongoose.connect("mongodb://localhost/blog", { useNewUrlParser: true });
 const app = express();
 const port = 3000;
@@ -14,25 +15,30 @@ app.use(bodyParser.json());
 
 app.get("/authors", (req, res) => {
   (async () => {
-    let authors = await Author.find();
-    res.send(authors);
+    await Author.find()
+      .populate("posts")
+      .exec((err, author) => {
+        res.status(200).send(author);
+      });
   })();
 });
 
 app.post("/post", (req, res) => {
-  console.log(req.body);
   (async () => {
-    const newPost = new Post({
-      content: "this is a post content",
-      author: "5fbd0f1ab5b6f4585f9c22d3",
+    let author = await Author.findById(req.body.authorID).exec();
+    const post = await Post.create({
+      content: req.body.content,
+      author: author._id,
     });
-    await newPost.save();
+    res.status(201).send(post);
+    author.posts.push(post._id);
+    await author.save();
   })();
 });
 
 app.get("/posts", (req, res) => {
   (async () => {
-    await Post.findOne()
+    await Post.find()
       .populate("author")
       .exec(function (err, post) {
         res.send(post);
